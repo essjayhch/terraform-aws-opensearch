1a86f52cd5edd7f2f280dae70cd8506e1f42a10f
Stuart Harland <essjayhch@users.noreply.github.com>
2024-07-30 12:17:54 +0100

    Update data.tf
    Signed-off-by: Stuart Harland <essjayhch@users.noreply.github.com>

--- data.tf
+++ data.tf
@@ -24,8 +24,7 @@ data "aws_iam_policy_document" "allow_logging" {
   statement {
     actions = [
       "logs:CreateLogStream",
-      "logs:PutLogEvents",
-      "logs:PutLogEventsBatch",
+      "logs:PutLogEvents"
     ]
 
     resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:${local.log_prefix}/*:*"]
